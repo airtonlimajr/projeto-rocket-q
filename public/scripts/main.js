aabdffa43b2d1d646230d8313796ac70928d71f6
airtonlimajr <airtonlimajr@outlook.com>
2021-08-16 23:14:38 -0300

    Aula 4.1

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,27 @@
-import { Modal } from './modal';
+import Modal from './modal.js';
 
 const modal = Modal()
 
-modal.open()
+const modalTitle = document.querySelector('.modal h2')
+
+const modalDescription = document.querySelector('.modal p')
+
+const modalButton = document.querySelector('.modal button')
+
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+checkButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        modal.open()
+
+    })
+
+})
+
+const deleteButton = document.querySelectorAll('.actions a.delete')
+
+deleteButton.forEach(button => {
+    button.addEventListener('click', event => {
+        modal.open()
+    })
+})
