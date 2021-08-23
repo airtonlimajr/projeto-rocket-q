4b2ea8fa956e4b098b2a90b78a1852cd6e01ddc2
airtonlimajr <airtonlimajr@outlook.com>
2021-08-22 22:29:57 -0300

    Aula 5 - Finished

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,18 +1,20 @@
-
 export default function Modal(){
 
-    const cancelButton = document.querySelector('.button.cancel');
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel')
 
-    cancelButton.addEventListener('click', close)
+    cancelButton.addEventListener("click", close)
 
     function open(){
-        document.querySelector('.modal-wrapper').classList.add('active')
+        //funcionalidade de atribuir a classe active para a modal
+        modalWrapper.classList.add("active")
     }
     function close(){
-        document.querySelector('.modal-wrapper').classList.remove('active')
+        //funcionalidade de remover a classe active da modal
+        modalWrapper.classList.remove("active")
     }
 
-    return {
+    return{
         open,
         close
     }
--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 const express = require('express')
-
 const route = require('./route')
+const path = require('path')
 
 const server = express()
 
-const path = require('path')
-
 server.set('view engine', 'ejs')
 
-server.use(express.static('public'))
+server.use(express.static("public"))
 
 server.set('views', path.join(__dirname, 'views'))
 
+server.use(express.urlencoded({extended: true}))
+
 server.use(route)
 
-server.listen(3000, () => console.log('Listening on port 3000'))
+server.listen(3000, () => console.log("RODANDO"))
